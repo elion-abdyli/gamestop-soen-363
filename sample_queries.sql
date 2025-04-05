aee8e6365a726a33a3b0f70793fd0bd2e526750e
Boudour Bannouri <131230020+Bou1103@users.noreply.github.com>
2025-04-04 22:01:03 -0400

    hard coded view+ constraints

--- sample_queries.sql
+++ sample_queries.sql
@@ -278,3 +278,58 @@ WHERE NOT EXISTS (
     FROM Symbol s2
     WHERE s2.quote_asset = s1.base_asset
 );
+
+-------------------------------------------------------
+-- View that has a hard-coded criteria
+-- Create a view that shows trading activity for a specific asset (hard-coded to 'BTC')
+CREATE OR REPLACE VIEW bitcoin_trading_activity AS
+SELECT 
+    market_id,
+    base_asset,
+    quote_asset,
+    time_utc,
+    price
+FROM Event
+WHERE base_asset = 'BTC'  -- Hard-coded value
+   OR quote_asset = 'BTC'; -- Hard-coded value
+
+SELECT * FROM bitcoin_trading_activity;
+
+-- Modify the hard-coded value in the view definition to get a different result:
+CREATE OR REPLACE VIEW bitcoin_trading_activity AS
+SELECT 
+    market_id,
+    base_asset,
+    quote_asset,
+    time_utc,
+    price
+FROM Event
+WHERE base_asset = 'ETH'  -- Changed hard-coded value
+   OR quote_asset = 'ETH'; -- Changed hard-coded value
+
+SELECT * FROM bitcoin_trading_activity;
+
+-------------------------------------------------------
+--- Overlap Constraint Query ---
+-- This query demonstrates assets that overlap in their usage as both base and quote currencies
+SELECT 
+    s1.base_asset AS asset_id,
+    'Used as both base and quote in different pairs' AS constraint_type,
+    COUNT(DISTINCT s1.quote_asset) AS trading_paths
+FROM Symbol s1
+JOIN Symbol s2 ON s1.base_asset = s2.quote_asset
+GROUP BY s1.base_asset
+ORDER BY trading_paths DESC;
+
+--- Covering Constraint Query ---
+-- This query checks if all assets in the system are covered by at least one market
+SELECT 
+    a.asset_id,
+    CASE 
+        WHEN COUNT(ma.market_id) = 0 THEN 'Not covered by any market (CONSTRAINT VIOLATION)'
+        ELSE 'Covered by ' || COUNT(ma.market_id) || ' market(s)'
+    END AS coverage_status
+FROM Asset a
+LEFT JOIN Market_Asset ma ON a.asset_id = ma.asset_id
+GROUP BY a.asset_id
+ORDER BY COUNT(ma.market_id);
