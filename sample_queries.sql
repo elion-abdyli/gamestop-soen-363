f684f6569ab2e132b35a2fd8edd4e64dc8ccfd9d
Boudour Bannouri <131230020+Bou1103@users.noreply.github.com>
2025-04-04 22:09:39 -0400

    division operator

--- sample_queries.sql
+++ sample_queries.sql
@@ -333,3 +333,41 @@ FROM Asset a
 LEFT JOIN Market_Asset ma ON a.asset_id = ma.asset_id
 GROUP BY a.asset_id
 ORDER BY COUNT(ma.market_id);
+
+-------------------------------------------------------
+--- Two implementations of the division operator ---
+-- both quesries for "Find markets that list all available assets"
+-- Ex 1 : regular nested query using NOT IN
+SELECT 
+    m.market_id,
+    m.name
+FROM Market m
+WHERE NOT EXISTS (
+    -- For each asset
+    SELECT 1
+    FROM Asset a
+    WHERE NOT EXISTS (
+        -- Check if it's associated with this market
+        SELECT 1
+        FROM Market_Asset ma
+        WHERE ma.market_id = m.market_id
+        AND ma.asset_id = a.asset_id
+    )
+);
+
+-- Ex 2 : Implementation using EXCEPT
+SELECT 
+    m.market_id,
+    m.name
+FROM Market m
+WHERE NOT EXISTS (
+    -- Find assets not listed in this market using EXCEPT
+    SELECT a.asset_id
+    FROM Asset a
+    EXCEPT
+    SELECT ma.asset_id
+    FROM Market_Asset ma
+    WHERE ma.market_id = m.market_id
+);
+
+
